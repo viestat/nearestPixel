5fd7cf17ca4632eada86356f5e7e8ba7fd3309ef
Andrés C. Viesca Ruiz <andres.viescar@gmail.com>
2019-07-13 18:19:04 +0200

    feat: Add util function to parse raw input

---
+++ src/util.ts
@@ -0,0 +1,29 @@
+/**
+ * Parses the input from a string to an object containig the elemnts for execution
+ * @param input - Raw string containing:
+ * The number of test cases t (1≤t≤1000) in the first line of input
+ * t test cases separated by an empty line, where each is structured as follows:
+ * - The first line has a pair of numbers n, m separated by a single space, 1<=n <=182, 1<=m<=182.
+ * - The following n lines of the test case is the description of one line of the bitmap.
+ * @returns Object with parsed elements
+ */
+const parseInput = (input: string) => {
+  const cases = [];
+  const lines = input.split("\n");
+  const t = parseInt(lines[0], 10);
+  const rawCases = lines.slice(1).join("\n").split("\n\n");
+  for (let i = 0; i < t; i++) {
+    const caseLines = rawCases[i].split("\n");
+    const [n, m] = caseLines[0].split(" ").map((val) => parseInt(val, 10));
+    const matrix = caseLines.slice(1).map((line) => {
+      return line.split("").map((val) => parseInt(val, 10));
+    });
+    cases.push({n, m, matrix});
+  }
+  return {
+    cases,
+    t,
+  };
+};
+
+export { parseInput };
---
+++ test/util.test.ts
@@ -0,0 +1,110 @@
+import { parseInput } from "../src/util";
+
+describe("Utilities", () => {
+  describe("parseInput", () => {
+    test("Should parse whith one case", () => {
+      const input = "1\n3 4\n0001\n0011\n0110";
+      const expected = {
+        cases: [{
+          m: 4,
+          matrix: [
+            [0, 0, 0, 1],
+            [0, 0, 1, 1],
+            [0, 1, 1, 0],
+          ],
+          n: 3,
+        }],
+        t: 1,
+      };
+      const result = parseInput(input);
+      expect(result).toEqual(expected);
+    });
+    test("Should parse whith two cases", () => {
+      const input = "2\n3 3\n000\n001\n011\n\n3 4\n0001\n0011\n0110";
+      const expected = {
+        cases: [
+          {
+            m: 3,
+            matrix: [
+              [0, 0, 0],
+              [0, 0, 1],
+              [0, 1, 1],
+            ],
+            n: 3,
+          },
+          {
+            m: 4,
+            matrix: [
+              [0, 0, 0, 1],
+              [0, 0, 1, 1],
+              [0, 1, 1, 0],
+            ],
+            n: 3,
+          },
+        ],
+        t: 2,
+      };
+      const result = parseInput(input);
+      expect(result).toEqual(expected);
+    });
+    describe("Should parse whith multiple cases", () => {
+      const genInput = (t: number) => {
+        let res = `${t}`;
+        const str = "\n3 4\n0001\n0011\n0110";
+        for (let i = 0; i < t; i++) {
+          res = `${res}${str}\n`;
+        }
+
+        return res.trim();
+      };
+      const genExpectedCases = (t: number) => {
+        return new Array(t).fill({
+          m: 4,
+          matrix: [
+            [0, 0, 0, 1],
+            [0, 0, 1, 1],
+            [0, 1, 1, 0],
+          ],
+          n: 3,
+        });
+      };
+      const testCases = [
+        {
+          expected: {
+            cases: genExpectedCases(20),
+            t: 20,
+          },
+          input: genInput(20),
+        },
+        {
+          expected: {
+            cases: genExpectedCases(400),
+            t: 400,
+          },
+          input: genInput(400),
+        },
+        {
+          expected: {
+            cases: genExpectedCases(1000),
+            t: 1000,
+          },
+          input: genInput(1000),
+        },
+      ];
+
+      const testGen = (spec: { input: any; expected: any; }, i: number) => {
+        const { input, expected} = spec;
+        test(`Case ${i + 1}: Should parse whith ${expected.t} cases`, () => {
+          const result = parseInput(input);
+          expect(result).toEqual(expected);
+        });
+      };
+
+      testCases.forEach(testGen);
+    });
+    test("Should fail if input has wrong shape", () => {
+      const input = "2\n4 3\n0001\n0011\n0110";
+      expect(() => { parseInput(input); }).toThrow();
+    });
+  });
+});
